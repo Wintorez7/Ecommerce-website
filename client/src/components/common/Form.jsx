65f03433b570cf9ef57bab6d6ce48b8666b43e8b
Wintorez7 <liordx76@gmail.com>
2025-01-26 15:42:31 +0530

    adding form functionality

--- client/src/components/common/Form.jsx
+++ client/src/components/common/Form.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import {Label} from '../ui/label';
 
 const CommForm = ({formControls}) => {
+
+    function renderInputByComponentType(getControlItem){
+        let element = null;
+        switch (getControlItem.componentType) {
+            case 'input':
+                element = <Input
+                name={getControlItem.name}
+                placeholder={getControlItem.placeholder}
+                id={getControlItem.name}
+                type={getControlItem.type}
+                />
+                break;
+        
+            default:
+                break;
+        }
+    }
+
   return (
     <div>
         <form>
@@ -10,7 +28,7 @@ const CommForm = ({formControls}) => {
                     formControls.map(controlItem => <div className='grid w-full gap-1.5' key={controlItem.name}>
                         <Label className='mb-1'>{controlItem.Label}</Label>
                         {
-                            
+                            renderInputByComponentType(controlItem) 
                         }
                     </div>)
                 }
